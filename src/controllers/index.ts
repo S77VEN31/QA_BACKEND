446a53b7be145fa68f70c945eac362def921962b
Daniel Granados <dandiegogranados@yahoo.com>
2024-10-06 16:28:44 -0600

    Report endpoints

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
 // Department Controller
 import { getDepartments } from "./department.controller";
 // Report Controller
-import { getReportDetail } from "./report.controller";
+import { getReportDetail, getReportTotal } from "./report.controller";
 
-export { getDepartments, getReportDetail };
+export { getDepartments, getReportDetail, getReportTotal };
--- src/controllers/report.controller.ts
+++ src/controllers/report.controller.ts
@@ -5,21 +5,34 @@ import { pool } from "../database";
 // Utils
 import { formatDate } from "../utils";
 
+// Get report details. There can be filters on ID, date range, and department.
+// It gets the report details from the database and sends them as a response.
+// The response is an array of objects, each object representing a row in the report.
+// It receives a start range from where its going to start getting the data and a limit range
 const getReportDetail = async (req: Request, res: Response) => {
   try {
-    let { date, IDCard, departmentID, startRange, endRange } = req.query;
+    let { date, endDate, IDCard, departmentID, startRange, limitRange } =
+      req.query;
 
     const formattedDate = formatDate(date as string);
+    const formattedEndDate = formatDate(endDate as string);
     const idCardNumber = IDCard ? parseInt(IDCard as string, 10) : null;
     const departmentIdNumber = departmentID
       ? parseInt(departmentID as string, 10)
       : null;
     const start = startRange ? parseInt(startRange as string, 10) : 0;
-    const end = endRange ? parseInt(endRange as string, 10) : 100;
+    const limit = limitRange ? parseInt(limitRange as string, 10) : 100;
 
     const result = await pool.query(
-      "SELECT * FROM getquincenas($1::DATE, $2::INT, $3::SMALLINT, $4::INT, $5::INT)",
-      [formattedDate, idCardNumber, departmentIdNumber, start, end]
+      "SELECT * FROM getquincenas($1::DATE, $2::DATE, $3::INT, $4::SMALLINT, $5::INT, $6::INT)",
+      [
+        formattedDate,
+        formattedEndDate,
+        idCardNumber,
+        departmentIdNumber,
+        start,
+        limit,
+      ]
     );
 
     res.status(200).json(result.rows);
@@ -29,4 +42,28 @@ const getReportDetail = async (req: Request, res: Response) => {
   }
 };
 
-export { getReportDetail };
+// Get total reports. There can be filters on ID, date range, and department.
+const getReportTotal = async (req: Request, res: Response) => {
+  try {
+    let { date, endDate, IDCard, departmentID } = req.query;
+
+    const formattedDate = formatDate(date as string);
+    const formattedEndDate = formatDate(endDate as string);
+    const idCardNumber = IDCard ? parseInt(IDCard as string, 10) : null;
+    const departmentIdNumber = departmentID
+      ? parseInt(departmentID as string, 10)
+      : null;
+
+    const result = await pool.query(
+      "SELECT * FROM getquincenastotal($1::DATE, $2::DATE, $3::INT, $4::SMALLINT)",
+      [formattedDate, formattedEndDate, idCardNumber, departmentIdNumber]
+    );
+
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error getting report detail");
+  }
+};
+
+export { getReportDetail, getReportTotal };
